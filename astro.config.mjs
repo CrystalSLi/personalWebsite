e5140c1fb173653e98fc2123d0734dd18d39b1fd
Crystal Li <crystalli@Crystals-MacBook-Pro.local>
2023-02-13 15:10:38 -0800

    fixing up astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://CrystalSLi.github.io',
+	base: '/personalWebsite',
 	integrations: [mdx(), sitemap()],
 });
